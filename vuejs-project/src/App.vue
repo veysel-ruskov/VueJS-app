20a3250ffaad09e299416ed340fa15a66059c98c
veysel-ruskov98 <77992122+veysel-ruskov98@users.noreply.github.com>
2022-01-04 20:00:20 +0200

    Initial commit

---
+++ vuejs-project/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="cards">
+    <p class="cardName">{{element}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  data() {
+    return{
+      element : []
+    }
+  },
+  components: {
+   // HelloWorld,
+  },
+  async created() {
+    var response = await fetch('https://greet.bg/wp-json/wc/store/products?page=1')
+    var data = await response.json();
+    //console.log(data[0].images[0].src)
+    for (let i = 0; i < data.length; i++) {
+       this.element = data[i]
+      console.log(this.element)
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
