dd85f4659481c847d1f249cd443ae0a565750b97
veysel-ruskov98 <77992122+veysel-ruskov98@users.noreply.github.com>
2022-01-05 12:08:10 +0200

    Update

--- vuejs-project/src/App.vue
+++ vuejs-project/src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div>
     <div class="cards">
-    <p class="cardName">{{element}}</p>
+      <div v-for="el in data" :key="el.id">
+        <img class="imgCard" :src="el.images[0].src" alt="Image" />
+        <p class="cardName">{{ el.name }}</p>
+        <p class="cardDescr">{{ el.description }}</p>
+        <p class="cardCateg">{{ el }}</p>     
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "App",
   data() {
-    return{
-      element : []
-    }
+    return {
+      data: [],
+    };
   },
   components: {
-   // HelloWorld,
+    // HelloWorld,
   },
   async created() {
-    var response = await fetch('https://greet.bg/wp-json/wc/store/products?page=1')
-    var data = await response.json();
-    //console.log(data[0].images[0].src)
-    for (let i = 0; i < data.length; i++) {
-       this.element = data[i]
-      console.log(this.element)
-    }
-  }
+    var response = await fetch(
+      "https://greet.bg/wp-json/wc/store/products?page=1"
+    );
+    this.data = await response.json()
+  },
 };
 </script>
 
