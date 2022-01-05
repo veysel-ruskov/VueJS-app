5c252b64ed3f8e1d4c3a224919fded4b04e0e10e
veysel-ruskov98 <77992122+veysel-ruskov98@users.noreply.github.com>
2022-01-05 15:30:19 +0200

    Update
    Added button ,hover and design.

--- vuejs-project/src/App.vue
+++ vuejs-project/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <div class="cards">
-      <div v-for="el in data" :key="el.id">
+    <div class="cards_container">
+      <div v-for="el in data" :key="el.id" class="cards">
         <img class="imgCard" :src="el.images[0].src" alt="Image" />
-        <p class="cardName">{{ el.name }}</p>
-        <p class="cardDescr">{{ el.description }}</p>
-        <p class="cardCateg">{{ el }}</p>     
+        <h3 class="cardName">{{ el.name }}</h3>
+        <p class="cardDescr" v-if="el.description != ''">{{ el.description }}</p>
+        <p class="cardCategorie" v-for="categrie in el.categories" :key="categrie.id">{{categrie.name}}</p>
+        <Btn class="btn_add_to_cart"></Btn>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Btn from "./components/add_to_cart_btn.vue"
 export default {
   name: "App",
   data() {
@@ -20,13 +22,14 @@ export default {
     };
   },
   components: {
-    // HelloWorld,
+    Btn,
   },
   async created() {
     var response = await fetch(
       "https://greet.bg/wp-json/wc/store/products?page=1"
     );
     this.data = await response.json()
+    console.log(this.data)
   },
 };
 </script>
@@ -39,5 +42,37 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.imgCard {
+  width: 230px;
+  height: 300px;
+  border-radius: 7%;
+}
+.cards_container {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  max-width: 100vw;
+  margin: 0 auto;
+}
+.cards {
+  max-width: calc(100vw/6);
+}
+.btn_add_to_cart {
+  display: none;
+}
+.cards:hover {
+  border: 2px solid ;
+  border-color: red;
+}
+.cards:hover .cardCategorie{
+  display: none;
+}
+.cards:hover .btn_add_to_cart {
+  display: block;
+}
+.imgCard:hover{
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
 }
 </style>
---
+++ vuejs-project/src/components/add_to_cart_btn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="btn_add_to_cart">
+      <p class="btn_text">Add to cart</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'add_to_cart_btn',
+}
+</script>
+
+<style scoped>
+.btn_add_to_cart {
+    background-color: blueviolet;
+    width: 9%;
+    height: 5%;
+    border-radius: 0%;
+    font-size: 20px;
+    cursor: pointer;
+    margin: auto;
+    border: 0px;
+}
+</style>
